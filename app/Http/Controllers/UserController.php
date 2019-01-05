2d90c9d97884c50d90b7b74dffe82a0581fb46c7
DeniesKresna <batkorumbawamedsos@gmail.com>
2019-01-06 02:19:14 +0700

    penambahan json pagination

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,12 +1,10 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\User;
 use App\Biodata;
-
 class UserController extends Controller
 {
     /**
@@ -19,7 +17,6 @@ class UserController extends Controller
         $users = User::all();
         return view('admin.user_show', ['users'=>$users]);
     }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -29,7 +26,6 @@ class UserController extends Controller
     {
         //
     }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -42,15 +38,12 @@ class UserController extends Controller
         $user->password = bcrypt($request->password);
         $user->photo = $request->username.'.jpg';
         $user->save();
-
         $request->merge(["user_id" => $user->id]);
         $biodata = Biodata::create($request->all());
         
-
         // $biodata = Biodata::create($request->all());
         // $biodata->user_id = $user->id;
         // $biodata->save();
-
         // Biodata::create([
         //     'phone' => $request->phone,
         //     'gender' => $request->gender,
@@ -59,12 +52,10 @@ class UserController extends Controller
         //     'place_of_birth' => $request->place_of_birth,
         //     'user_id' => $user->id,
         // ]);
-
         $request->file('photo')->move(public_path("/images/users/"), $user->username.'.jpg');
         
         return redirect()->route('user_show');
     }
-
     /**
      * Display the specified resource.
      *
@@ -75,7 +66,6 @@ class UserController extends Controller
     {
         //
     }
-
     /**
      * Show the form for editing the specified resource.
      *
@@ -86,7 +76,6 @@ class UserController extends Controller
     {
         //
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -98,14 +87,11 @@ class UserController extends Controller
     {
         $user = User::findOrFail($id);
         $user->update($request->all());
-
         // $biodata = Biodata::where('user_id', '=', $id);
         // $user->biodata->where('user_id', $id)->update($request->all());
         $user->biodata->update($request->all());
-
         return redirect()->route('user_show');
     }
-
     /**
      * Remove the specified resource from storage.
      *
@@ -116,10 +102,8 @@ class UserController extends Controller
     {
         $user = User::findOrFail($id);
         $user->delete();
-
         return redirect()->route('user_show');
     }
-
     public function getProfile($id)
     {   
         if(Auth::user()->id == $id)
@@ -129,30 +113,47 @@ class UserController extends Controller
         } 
         return back();
     }
-
     public function updateProfile(Request $request, $id)
     {
         $user = User::findOrFail($id);
         $user->update($request->all());
-
         $user->photo = $user->username.'.jpg';
         $user->save();
-
         $request->file('featured_image')->move(public_path("/images/users/"), $post->username.'.jpg');
-
         return redirect()->route('user_profile', $id)->with('message', 'Berhasil Mengubah Data');
     }
-
     public function changePhoto(Request $request, $id)
     {
         $user = User::findOrFail($id);
-           
         $user->photo = $user->username.'.jpg';
         $user->save();
-
         $request->file('photo')->move(public_path("/images/users/"), $user->username.'.jpg');
-
         return redirect()->route('user_profile', $id)->with('message', 'Berhasil Mengubah Foto Profil');
+    }
+    public function changePassword(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $password_lama = $request->password_lama;
+        $password_baru = $request->password_baru;
+        $konfirmasi_password = $request->konfirmasi_password;
+        if(Hash::check($password_lama, $user->password))
+        {
+            if($password_baru == $konfirmasi_password) 
+            {
+                $user->password = bcrypt($password_baru);
+                $user->save();
+                return redirect()->route('user_profile', $id)->with('message', 'Berhasil Mengubah Kata Sandi');
+            }
+            return back()->with('message', 'Kata Sandi Baru Dengan Konfirmasi Kata Sandi Baru Tidak Sama');
+        }
+        return back()->with('message', 'Kata Sandi Sekarang Salah');
+    }
+    
+    public function countUser()
+    {
+        $users = User::all();
+        $total_users = count($users);
+        return view('admin.index')->with(['total_users' => $total_users]);
     }
 
     public function jsondata(Request $request)
